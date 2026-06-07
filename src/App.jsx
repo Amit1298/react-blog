f22ad47ce5e935463ed23c3553f1f2e395612943
Amit <Amit1298>
2026-06-07 12:07:26 +0530

    counter implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import { Apple } from "./fruits/Apple";
 import Wrapper from "./Wrapper";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function handleDecrement() {
+    if (count > 0) setCount(count - 1);
+  }
+  function handleReset() {
+    setCount(0);
+  }
   return (
     <>
       <h1>Welcome to Vite + React</h1>
+      <h3>Count: {count >= 0 ? count : "not set"}</h3>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
+      <hr />
       <Wrapper>
         <Apple />
       </Wrapper>
